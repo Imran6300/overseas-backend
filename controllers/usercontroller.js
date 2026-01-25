8aa2f0d6628f4eb9964ce71879d02ed227549144
Imran6300 <syedimran8742@gmail.com>
2026-01-25 22:14:21 +0530

    added assessment route

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,7 @@
 module.exports.PostCounselingForm = (req, res) => {
   console.log(req.body)
 }
+
+module.exports.PostAssessmentForm = (req, res) => {
+  console.log(req.body)
+}
--- routes/userroute.js
+++ routes/userroute.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const UserRouter = express.Router()
 
-const { PostCounselingForm } = require("../controllers/usercontroller")
+const { PostCounselingForm, PostAssessmentForm } = require("../controllers/usercontroller")
 
 UserRouter.post("/api/counseling", PostCounselingForm)
+UserRouter.post("/api/assessment", PostAssessmentForm)
 
 module.exports = { UserRouter }
